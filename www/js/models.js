58b0d5ba43a695435e1c73d1361cfe61281d7c38
Jonas Masalskis <jonasm@inbox.lt>
2015-08-18 17:51:01 +0300

    Data injection for models

--- www/js/models.js
+++ www/js/models.js
@@ -1,11 +1,11 @@
 angular.module('hotvibes.models', ['ngResource', 'hotvibes.config'])
 
     .factory('User', function($resource, Config) {
-        return $resource(Config.API_URL_BASE + 'users/:id');
+        return $resource(Config.API_URL_BASE + 'users/:id', { id: '@id' });
     })
 
     .factory('Conversation', function($resource, Config) {
-        return $resource(Config.API_URL_BASE + 'users/:ownerId/conversations/:withUserId');
+        return $resource(Config.API_URL_BASE + 'users/:ownerId/conversations/:withUserId', { ownerId: '@ownerId', withUserId: '@id' });
     })
 
     .factory('Message', function($resource, Config) {
