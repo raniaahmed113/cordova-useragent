3ea643e4fa73396289a043d5b17312f598f55b51
Jonas Masalskis <jonasm@inbox.lt>
2015-11-28 20:34:13 +0200

    This file is auto-generated

--- www/js/config.js
+++
@@ -1,7 +0,0 @@
-angular.module('hotvibes.config', [])
-
-    .constant('Config', {
-        API_URL_BASE: 'https://api.flirtas.lt/',
-        API_CLIENT_ID: 2,
-        LANGUAGES: ["lv","ru"]
-    });
