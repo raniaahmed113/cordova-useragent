0756efad6bfdd87d10beafc5369eabb6b8815cb0
Jonas Masalskis <jonasm@inbox.lt>
2015-11-30 16:26:37 +0200

    Adapting to API changes. Closes #30

--- www/js/services.js
+++ www/js/services.js
@@ -110,8 +110,9 @@ angular.module('hotvibes.services', ['ionic', 'hotvibes.config'])
     .service('HttpInterceptor', function($rootScope, $window, $q, AuthService, Config, ErrorCode) {
         this.request = function(config) {
             if (config.url.startsWith(Config.API_URL_BASE)) {
-                var currentUser = AuthService.getCurrentUser();
+                config.headers['X-Client-Id'] = Config.API_CLIENT_ID;
 
+                var currentUser = AuthService.getCurrentUser();
                 if (currentUser != null) {
                     config.headers.Authorization = 'Bearer ' + currentUser.accessToken;
 
