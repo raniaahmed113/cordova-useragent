a857a5d3f3d93133301fb9b36c1412e230a76f48
Jonas Masalskis <jonasm@inbox.lt>
2016-05-12 13:54:38 +0300

    Further implementing support view

--- www/js/controllers/support.js
+++ www/js/controllers/support.js
@@ -1,18 +1,36 @@
 angular.module('hotvibes.controllers')
 
-    .controller('SupportCtrl', function($scope, $ionicLoading, __, SupportRequest) {
+    .controller('SupportCtrl', function($scope, $ionicLoading, $ionicHistory, __, SupportRequest) {
         $scope.supportRequest = new SupportRequest();
         $scope.supportRequest.submit = function() {
             if (!this.text) {
                 return;
             }
 
-            this.$save();
+            $ionicLoading.show({ template: __("Please wait") + '..' });
 
-            $ionicLoading.show({
-                template: __('Thank you, your message has been send. We will replay you with in 48 hours.'),
-                noBackdrop: true,
-                duration: 2000
+            var self = this;
+
+            self.$save().then(
+                function() {
+                    // Show success message
+                    $ionicLoading.show({
+                        template: __('Thank you, your message has been send. We will replay you with in 48 hours.'),
+                        noBackdrop: true,
+                        duration: 3000
+                    });
+
+                    // Reset the input field
+                    self.text = '';
+
+                    // Go back to previous window (if any)
+                    $ionicHistory.goBack();
+                },
+                function() {
+                    $scope.onError();
+                }
+            ).finally(function() {
+                $ionicLoading.hide();
             });
         };
     });
