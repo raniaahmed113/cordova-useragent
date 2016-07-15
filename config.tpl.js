61ca83641048ae369108d17eb1cb27b745d8e5db
Jonas Masalskis <jonasm@inbox.lt>
2016-07-15 15:51:26 +0300

    Forgot one more thing

--- config.tpl.js
+++ config.tpl.js
@@ -1,7 +1,7 @@
 angular.module('hotvibes.config', [])
 
     .constant('Config', {
-        API_URL_BASE: 'http://vertex-flirtas-api-jm.vagrantshare.com/',
+        API_URL_BASE: 'http://api.flirtas.dev/',
         API_CLIENT_ID: $apiClientId,
         LANGUAGES: $languages,
         FB_APP_ID: $fbAppId
