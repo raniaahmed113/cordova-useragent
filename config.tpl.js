8455a55151c90f12f61b7c534bddd6e5af44f40f
Jonas Masalskis <jonasm@inbox.lt>
2016-08-05 12:17:31 +0300

    Cleanup

--- config.tpl.js
+++
@@ -1,8 +0,0 @@
-angular.module('hotvibes.config', [])
-
-    .constant('Config', {
-        API_URL_BASE: 'http://api.flirtas.dev/',
-        API_CLIENT_ID: $apiClientId,
-        LANGUAGES: $languages,
-        FB_APP_ID: $fbAppId
-    });
