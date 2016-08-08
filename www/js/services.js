0ed10604e4bb40a90e5ef0460e04415cdc404d39
Jonas Masalskis <jonasm@inbox.lt>
2016-08-08 21:07:54 +0300

    Push notifications config

--- www/js/services.js
+++ www/js/services.js
@@ -159,23 +159,23 @@ angular.module('hotvibes.services', ['ionic', 'hotvibes.config'])
             deviceId = localStorage['deviceId'];
 
             push = PushNotification.init({
-                // android: {},
-                // ios: {
-                //     alert: true,
-                //     badge: true,
-                //     sound: true,
-                //     categories: {
-                //         newMessage: {
-                //             yes: {
-                //                 title: __('Reply'), callback: 'message.reply', destructive: false, foreground: false
-                //             },
-                //             no: {
-                //                 title: __('Mark as read'), callback: 'message.markAsRead', destructive: false, foreground: false
-                //             }
-                //         }
-                //     }
-                // },
-                // windows: {}
+                android: {},
+                ios: {
+                    alert: true,
+                    badge: true,
+                    sound: true,
+                    categories: {
+                        newMessage: {
+                            yes: {
+                                title: __('Reply'), callback: 'message.reply', destructive: false, foreground: false
+                            },
+                            no: {
+                                title: __('Mark as read'), callback: 'message.markAsRead', destructive: false, foreground: false
+                            }
+                        }
+                    }
+                },
+                windows: {}
             });
 
             push.on('registration', onDeviceRegistered);
